54c31dd79f095da997f46775beabe7f995e80d52
doge421 <lorenzonulli42@gmail.com>
2025-04-27 13:50:47 +0200

    Added some comments in ex01

--- ex01/srcs/Fixed.cpp
+++ ex01/srcs/Fixed.cpp
@@ -86,9 +86,13 @@ int Fixed::toInt(void)const
   return (this->_fpValue >> this->_fract_bits);
 }
 
+/* to convert from int to float, we have to divide the
+ * float value for (2 ^ fract_bits).*/
+
 float Fixed::toFloat(void)const
 {
   return ((float)this->_fpValue / (float)(1 << this->_fract_bits));
+  // return ((float)this->_fpValue / (float)pow(2,this->_fract_bits));
 }
 
 
